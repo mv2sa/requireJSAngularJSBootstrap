24d6a015d3dbce0649b0abfaddcc9c7bedc54c94
Marcos Santanna <marcosv.santanna@hotmail.com>
2014-07-18 16:40:22 -0400

    clean up, jquery bootstrap integration with angular

--- js/main.js
+++ js/main.js
@@ -7,7 +7,19 @@ requirejs.config({
     },
     shim : {
         "angular" : {"exports" : "angular"},
-    	"uiBootstrap" : {"deps" : "angular"}
+    	"uiBootstrap" : {"deps" : "angular"},
+        "bootstrap/affix":      { "exports": "$.fn.affix" }, 
+        "bootstrap/alert":      { "exports": "$.fn.alert" },
+        "bootstrap/button":     { "exports": "$.fn.button" },
+        "bootstrap/carousel":   { "exports": "$.fn.carousel" },
+        "bootstrap/collapse":   { "exports": "$.fn.collapse" },
+        "bootstrap/dropdown":   { "exports": "$.fn.dropdown" },
+        "bootstrap/modal":      { "exports": "$.fn.modal" },
+        "bootstrap/popover":    { "exports": "$.fn.popover" },
+        "bootstrap/scrollspy":  { "exports": "$.fn.scrollspy" },
+        "bootstrap/tab":        { "exports": "$.fn.tab"        },
+        "bootstrap/tooltip":    { "exports": "$.fn.tooltip" },
+        "bootstrap/transition": { "exports": "$.fn.transition" }
     },
 	priority: [
 		"angular"
@@ -16,9 +28,9 @@ requirejs.config({
 
 window.name = "NG_DEFER_BOOTSTRAP!";
 
-require(["angular", "app"], function (angular, app) {
+require(["angular", "app", "bootstrap/carousel", "bootstrap/dropdown", "bootstrap/transition"], function (angular, app) {
 
-    var $angularBoot = angular.element(document.getElementById('main'));
+    var $angularBoot = angular.element(document.getElementsByTagName('body')[0]);
 
     angular.element().ready(function() {
         angular.resumeBootstrap([app['name']]);
